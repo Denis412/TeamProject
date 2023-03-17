bb25e245d53ccb0b5129fd20ddf70d96bcb79547
Denis412 <d-a-malyshev@yandex,ru>
2023-03-18 00:25:50 +0300

    Добавление недостающих компонентов

---
+++ src/components/Home/MIAProductItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-item class="product block col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    <div class="wrapper q-pa-sm">
+      <MIAHeader
+        :id="product.id"
+        :image="product.image"
+        :title="product.title"
+      />
+      <MIAMainContent
+        :description="product.description"
+        :price="product.price"
+        :old_price="product.old_price"
+      />
+      <MIAFooter :product="product" />
+    </div>
+  </q-item>
+</template>
+
+<script setup>
+import MIAHeader from "../Product/MIAHeader.vue";
+import MIAMainContent from "../Product/MIAMainContent.vue";
+import MIAFooter from "../Product/MIAFooter.vue";
+
+const { product } = defineProps({
+  product: Object,
+});
+</script>
+
+<style lang="sass" scoped>
+.wrapper
+  background: #F6F9FF
+  border-radius: 26px
+.product
+  min-height: 504px
+  margin-bottom: 56px
+</style>

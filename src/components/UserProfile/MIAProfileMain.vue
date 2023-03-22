cf7d5f1e9f21bb27e1e3b11c9bf217bd435bbc9d
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2023-03-22 11:42:02 +0300

    Адаптив для товаров профиля, и проверка  наличия

--- src/components/UserProfile/MIAProfileMain.vue
+++ src/components/UserProfile/MIAProfileMain.vue
@@ -51,67 +51,23 @@
         >
       </div>
     </q-form>
-    <q-dialog v-model="products">
-      <q-list style="width: 80%;" class="q-mt-xl products row bg-white">
-    <div v-if="loading">Загрузка...</div>
-    <q-item
-      v-else
-      class="product q-pa-lg q-mb-lg row offset-lg-2 col-lg-8 col-12"
-      v-for="product in result?.products"
-      :key="product.id"
-    >
-      <q-item-section class="col-sm-4 col-xs-12">
-        <q-img :src="product.image" />
-      </q-item-section>
-      <div class="col-sm-8 col-xs-12 relative-position q-pl-lg">
-        <q-item-section class="product__title text-h5 q-mt-md text-weight-bold">
-          {{ product.title }}
-        </q-item-section>
-        <q-item-section class="q-my-md">
-          {{ product.description }}
-        </q-item-section>
-        <div class="price-area row q-ml-sm">
-          <div class="price col-4 text-h5 text-red">
-            От {{ product.price }} Р
-          </div>
-          <div
-            class="old-price text-h6 col-4 text-grey"
-            v-if="product.old_price"
-          >
-            От {{ product.old_price }} Р
-          </div>
-        </div>
-        <div class="absolute-bottom flex justify-end row q-ml-lg">
-          <q-btn
-            @click="deleteFromProducts(product.id)"
-            flat
-            class="col-5 offset-1 btn-tocart bg-primary"
-            label="Удалить"
-          />
-        </div>
-      </div>
-    </q-item>
-  </q-list>
+    <q-dialog class="row" v-model="products">
+      <Products :user="user"/>
     </q-dialog>
   </main>
 </template>
 
 <script setup>
-import { getProductsByUser } from "src/graphql-operations/queries";
-import { useQuery,useMutation } from "@vue/apollo-composable";
-import { removeProductFromProducts } from "src/graphql-operations/mutations";
 import { useQuasar } from "quasar";
 import { ref, computed, watch } from "vue";
+import Products from "./MIAProfileProducts.vue";
 import userApi from "../../sdk/user";
 
 const $q = useQuasar();
 
 const user = window.Clerk.user;
+
 const products = ref(false)
-const id = ref({id:user.id})
-
-const { result, loading, error, refetch } = useQuery(getProductsByUser,id);
-
 
 const form = ref({
   firstName: user.firstName,
@@ -121,16 +77,8 @@ const form = ref({
   password: user.password,
 });
 
-const { mutate: deleteProduct} = useMutation(removeProductFromProducts)
 
-const deleteFromProducts = async(id) =>{
-  const user = window.Clerk.user;
-  if (!user) return;
-  const { data } = await deleteProduct({
-    id: id,
-  });
-  refetch();
-}
+
 
 const updateProfile = () => {
   userApi.updateProfile(form.value);
@@ -140,3 +88,7 @@ const updateProfile = () => {
   });
 };
 </script>
+<style lang="sass">
+.no-wrap
+  flex-wrap: wrap
+</style>
---
+++ src/components/UserProfile/MIAProfileProducts.vue
@@ -0,0 +1,78 @@
+<template>
+      <q-list class="col-sm-10 col-12 q-mt-xl products row bg-white">
+        <q-btn class="offset-sm-11 offset-10" icon="close" size="30px" flat round dense v-close-popup />
+
+    <div style="margin: 0 auto;" class="q-pb-xl flex-center text-h5" v-show="!result?.products.length">
+      Список товаров пуст
+    </div>
+    <div v-if="loading">Загрузка...</div>
+    <q-item
+      v-else
+      class="product q-pa-lg q-mb-lg row col-lg-12 col-12 wrap"
+      v-for="product in result?.products"
+      :key="product.id"
+    >
+      <q-item-section class="col-sm-4 col-xs-12">
+        <q-img :src="product.image" />
+      </q-item-section>
+      <div class="col-sm-8 col-xs-12 relative-position q-pl-lg">
+        <q-item-section class="product__title text-h5 q-mt-md text-weight-bold">
+          {{ product.title }}
+        </q-item-section>
+        <q-item-section class="q-my-md">
+          {{ product.description }}
+        </q-item-section>
+        <div class="price-area row q-ml-sm">
+          <div class="price col-4 text-h5 text-red">
+            От {{ product.price }} Р
+          </div>
+          <div
+            class="old-price text-h6 col-4 text-grey"
+            v-if="product.old_price"
+          >
+            От {{ product.old_price }} Р
+          </div>
+        </div>
+        <div class="absolute-bottom flex justify-end row q-ml-lg">
+          <q-btn
+            @click="deleteFromProducts(product.id)"
+            flat
+            class="col-5 offset-1 btn-tocart bg-primary"
+            label="Удалить"
+          />
+        </div>
+      </div>
+    </q-item>
+  </q-list>
+</template>
+
+<script setup>
+import { getProductsByUser } from "src/graphql-operations/queries";
+import { useQuery,useMutation } from "@vue/apollo-composable";
+import { removeProductFromProducts } from "src/graphql-operations/mutations";
+import { defineProps,ref } from "vue";
+
+const { user } = defineProps({
+  user:Object,
+})
+
+const id = ref({id:user.id})
+
+const { result, loading, error, refetch } = useQuery(getProductsByUser,id);
+
+const { mutate: deleteProduct} = useMutation(removeProductFromProducts)
+
+const deleteFromProducts = async(id) =>{
+  const user = window.Clerk.user;
+  if (!user) return;
+  const { data } = await deleteProduct({
+    id: id,
+  });
+  refetch();
+}
+</script>
+
+<style lang="sass" scoped>
+.q-dialog__inner--minimized > div
+  width: 80%
+</style>

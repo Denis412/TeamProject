5e995202278f1b8cd0fd85045e81316d1e07a3cd
NightDriver999 <andrey.ionov2013@yandex.ru>
2023-03-24 11:25:39 +0300

    Стилизованный селект

---
+++ src/layouts/MIASelect.vue
@@ -0,0 +1,21 @@
+<template>
+    <div style="max-width: 350px">
+        <q-select class="select-input" borderless v-model="model" :options="options" label="Сортировка" />
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.select-input {
+  max-width: 531px;
+  background: rgba(83, 141, 255, 0.05);
+  border-radius: 10px;
+  font-size: 23px;
+  padding-left: 15px;
+}
+</style>
